3747fc7904d4f10711a6f328101769a0977f6370
user1hunter <user1hunter@gmail.com>
2022-03-02 09:58:10 -0600

    Phase 3 Done

--- Program.cs
+++ Program.cs
@@ -43,7 +43,7 @@ namespace Quest
       int maxAwesomeness = 100;
 
       //Get Players Name
-      Console.WriteLine("Please enter your name.");
+      Console.WriteLine("Please enter your name, Adventurer.");
       string name = Console.ReadLine();
 
       // Make a new "Adventurer" object using the "Adventurer" class
@@ -60,25 +60,38 @@ namespace Quest
                 favoriteBeatle
             };
 
-      // Loop through all the challenges and subject the Adventurer to them
-      foreach (Challenge challenge in challenges)
-      {
-        challenge.RunChallenge(theAdventurer);
-      }
+      bool gameActive = true;
 
-      // This code examines how Awesome the Adventurer is after completing the challenges
-      // And praises or humiliates them accordingly
-      if (theAdventurer.Awesomeness >= maxAwesomeness)
+      while (gameActive == true)
       {
-        Console.WriteLine("YOU DID IT! You are truly awesome!");
-      }
-      else if (theAdventurer.Awesomeness <= minAwesomeness)
-      {
-        Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
-      }
-      else
-      {
-        Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+        // Loop through all the challenges and subject the Adventurer to them
+        foreach (Challenge challenge in challenges)
+        {
+          challenge.RunChallenge(theAdventurer);
+        }
+
+        // This code examines how Awesome the Adventurer is after completing the challenges
+        // And praises or humiliates them accordingly
+        if (theAdventurer.Awesomeness >= maxAwesomeness)
+        {
+          Console.WriteLine("YOU DID IT! You are truly awesome!");
+        }
+        else if (theAdventurer.Awesomeness <= minAwesomeness)
+        {
+          Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
+        }
+        else
+        {
+          Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+        }
+
+        Console.WriteLine("Would you like to play again? Y or N.");
+        string repeatGame = Console.ReadLine();
+
+        if (repeatGame != "Y")
+        {
+          gameActive = false;
+        }
       }
     }
   }
